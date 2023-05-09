8780a821fddc8f5eeb2095fc547718a2efafe6f5
Kent Erik <kent.erik.hole@outlook.com>
2023-05-09 17:23:37 +0200

    adding get post

--- src/js/api/post.js
+++ src/js/api/post.js
@@ -1,11 +1,15 @@
-import { baseURL } from "./base";
+import { baseURL, endPoint_listings } from '../data/base';
+import { save } from '../storage/save'
 
 export async function feedpost(){
-    const response = await fetch(baseURL, {
-        method: 'get',
-        
+    const response = await fetch(baseURL+endPoint_listings,  {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        }
     })
     if(response.ok){
-        console.log(await response.json())
+        const data = await response.json()
+        save('post' ,data)
     }
 }
